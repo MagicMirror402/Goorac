2d139bd4be4e50c5fc1e3e218b922396ac1c0ae1
MagicMirror402 <81296450+MagicMirror402@users.noreply.github.com>
2021-03-24 15:13:48 +0530

    Add files via upload

---
+++ time.js
@@ -0,0 +1,25 @@
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+var d = new Date();
+document.getElementById("date").innerHTML = d;
+
+
+
+  var goFS = document.getElementById("goFS");
+  goFS.addEventListener("click", function() {
+      document.body.requestFullscreen();
+  }, false);
